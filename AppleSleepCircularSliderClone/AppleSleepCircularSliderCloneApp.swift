67751efa782595d1ee9dc4654161eb9a16884a7d
Ahmed Hamad <Ahmedabdulsalam7@outlook.com>
2024-11-19 16:27:48 +0200

    Initial Commit

---
+++ AppleSleepCircularSliderClone/AppleSleepCircularSliderCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleSleepCircularSliderCloneApp.swift
+//  AppleSleepCircularSliderClone
+//
+//  Created by Ahmed Hamad on 19/11/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleSleepCircularSliderCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
